9039a53820d4aa545ca8c4786833faa5119c5e64
ParkDooWon <pass345@naver.com>
2019-12-06 03:00:06 +0900

    feat : Main.java 구현

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Racing Game 시작을 위한 Main 클래스
+ * 
+ * version : 1.0
+ * 
+ * 2019.12.06
+ * 
+ * @author 박두원
+ * 
+ */
+import domain.RacingGame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		RacingGame game = new RacingGame();
+		game.startGame();
+	}
+	
+}
