5d2b116e071cbf9e43eab470dad840c4ed12be02
ParkDooWon <pass345@naver.com>
2019-12-06 03:01:47 +0900

    feat : Car.java 구현

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,12 +1,80 @@
+/*
+ * Class Name : Car.java
+ * 
+ * version : 1.0
+ * 
+ * 2019.12.06
+ * 
+ * @author �ڵο�
+ * 
+ */
+
 package domain;
 
+import java.util.Random;
+
 public class Car {
-    private final String name;
-    private int position = 0;
+	private static final int MAX_NUM = 10;
+	private static final int MOVE = 4;
+	private static final int STAY = 3;
+	private static final String IS = " : ";
+	private final String name;
+	private int position = 0;
+	private int biggerPosition = 0;
+	private int randomNum;
+	private String positions;
+	Random random = new Random();
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    // 추가 기능 구현
+	public void move() {
+		randomNum = makeRandomNumber();
+		if (randomNum >= MOVE) {
+			this.position++;
+		} else if (randomNum <= STAY) {
+			this.position += 0;
+		}
+	}
+	
+	public void printResult(char shape) {
+		System.out.println(this.name + IS + printPosition(shape));
+	}
+
+	public int findWinnerPosition(int tempPosition) {
+		biggerPosition = 0;
+		if (this.position < tempPosition) {
+			biggerPosition = tempPosition;
+		} else if (this.position >= tempPosition) {
+			biggerPosition = this.position;
+		}
+		return biggerPosition;
+	}
+
+	public boolean isWinner(int winnerPosition) {
+		boolean status = false;
+		if (this.position == winnerPosition) {
+			status = true;
+		}
+		return status;
+	}
+
+	public String getWinnerName() {
+		return this.name;
+	}
+	
+	private int makeRandomNumber() {
+		int tempRandomNum = random.nextInt(MAX_NUM);
+		return tempRandomNum;
+	}
+
+	private String printPosition(char shape) {
+		positions = "";
+		for (int i = 0; i < position; i++) {
+			positions += shape;
+		}
+		return positions;
+	}
+
 }
