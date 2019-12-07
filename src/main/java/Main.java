789e11d9795fe2e740f93ec52d14e0fb5cf091e5
ParkDooWon <pass345@naver.com>
2019-12-08 01:35:03 +0900

    feat : Main.java 생성

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+/*
+ * 클래스명 : Main
+ * 
+ * version : 0.1
+ * 
+ * 날짜 : 2019.12.08
+ * 
+ * 작성자 : ParkDooWon
+ */
+import domain.RacingGame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		RacingGame game = new RacingGame();
+		game.startGame();
+
+	}
+
+}
